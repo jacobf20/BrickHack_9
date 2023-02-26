5f5e845d87199f7a787ead02f67e5e63980c07ca
evanfp582 <evanfp582@gmail.com>
2023-02-26 06:14:48 -0500

    Form done and object created

--- client/src/Pages/UploadRun.js
+++ client/src/Pages/UploadRun.js
@@ -3,10 +3,7 @@ import {
   Grid,
   TextField,
   Paper,
-  Item,
-  Button,
-  Box,
-  Container
+  Button
 } from '@material-ui/core';
 import '../App.css';
 import InputLabel from '@mui/material/InputLabel';
@@ -17,154 +14,248 @@ import Select from '@mui/material/Select';
 
 
 const UploadRun = (props) => {
-  console.log(props);
-  
-  const formCss = {
-    display: 'flex',alignItems: 'center',  justifyContent: 'center'
+
+  const [date, setDate] = useState("")
+  const [sleepTime, setSleepTime] = useState("")
+  const [time, setTime] = useState("")
+  const [inputField, setInputField] = useState({
+    "Name": "",
+    "Type": "",
+    "Pace": {
+      "Minutes": undefined,
+      "Seconds": undefined,
+    },
+    "Weather": "",
+    "Miles": undefined,
+    "Time": {
+      "Minutes": 0,
+      "Seconds": 0
+    },
+    "AHR": undefined,
+    "BeforeRating": undefined,
+    "DuringRating": undefined,
+    "Sleep": {
+      "Hours": 0,
+      "Minutes": 0,
+    },
+    "RHR": undefined,
+    "Notes": "",
+    "Date": {
+      "Month": 0,
+      "Day": 0,
+      "Year": 0
+    }
+  })
+
+  const inputsHandler = (e) =>{
+    setInputField({...inputField, [e.target.id]: e.target.value} )
   }
 
-    return (<>
-      <Paper style={{margin:"25px", padding:"25px", backgroundColor:"#FAF9F6"}}>
-        <Grid container spacing={3}>
-          <Grid item xs={8}>
-            <TextField
-              style={{ "font-size": "400px"}}
-              id="runName"
-              label="Title of Run"
-              variant="standard"
-              size='large'
-              fullWidth 
-              inputProps={{style: {fontSize: 40}}}
-              // autoComplete="current-password"
-            />
-          </Grid>
-          <Grid item xs={4}>
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Type of Run</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="typeOfRun"
-                // value={}
-                label="Type of Run"
-                // onChange={handleChange}
-              >
-                <MenuItem value={10}>Training Run</MenuItem>
-                <MenuItem value={20}>Long Run</MenuItem>
-                <MenuItem value={30}>Race</MenuItem>
-                <MenuItem value={30}>Workout</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
+  const submitButton = () =>{
+    console.log("um what", inputField);
+  }
 
-          <Grid item xs={4} >
-            <TextField
-                id="weather"
-                label="Weather"
-                // type="password"
-                autoComplete="current-password"
-                variant="filled"
-              />
-          </Grid>
-          <Grid item xs={8}  >
-            <TextField
-                id="date"
-                label="Date"
-                // type="password"
-                // autoComplete="current-password"
-                variant="filled"
-              />
-          </Grid>
-          <Grid item xs={4}  >
-            <TextField
-                id="miles"
-                label="Miles"
-                // type="password"
-                // autoComplete="current-password"
-                variant="filled"
-              />
-          </Grid>
-          <Grid item xs={4}  >
-            <TextField
-                id="time"
-                label="Time"
-                // type="password"
-                // autoComplete="current-password"
-                variant="filled"
-              />
-          </Grid>
-          <Grid item xs={4}  >
-            <TextField
-                id="pace"
-                label="Pace"
-                // type="password"
-                // autoComplete="current-password"
-                variant="filled"
-                disabled
-              />
-          </Grid>
-          <Grid item xs={4}  >
-            <TextField
-                id="ahr"
-                label="Average Heart Rate"
-                // type="password"
-                // autoComplete="current-password"
-                variant="filled"
-              />
-          </Grid>
-          <Grid item xs={4}  >
-            <TextField
-                id="beforeRating"
-                label="Before Rating"
-                // type="password"
-                // autoComplete="current-password"
-                variant="filled"
-              />
-          </Grid>
-          <Grid item xs={4}  >
-            <TextField
-                id="afterRating"
-                label="After Rating"
-                // type="password"
-                // autoComplete="current-password"
-                variant="filled"
-              />
-          </Grid>
-          <Grid item xs={4}  >
-            <TextField
-                id="sleep"
-                label="Sleep"
-                // type="password"
-                // autoComplete="current-password"
-                variant="filled"
-              />
-          </Grid>
-          <Grid item xs={4}  >
-            <TextField
-                id="rhr"
-                label="Resting Heart Rate"
-                // type="password"
-                // autoComplete="current-password"
-                variant="filled"
-              />
-          </Grid>
-          <Grid item xs={12}  >
+  const parseTime = (e) =>{
+    setTime(e.target.value)
+    let yaga = e.target.value.split(":")
+    let newTime = {"Minutes": Number(yaga[0]), "Seconds": Number(yaga[1])}
+    setInputField( {
+      ...inputField, 
+      "Time": newTime })
+  }
+
+  const parseSleepTime = (e) =>{
+    setSleepTime(e.target.value)
+    let yaga = e.target.value.split(":")
+    let newSleepTime = {"Hours": yaga[0], "Minutes": yaga[1]}
+    setInputField( {
+      ...inputField, 
+      "Sleep": newSleepTime })  }
+
+  const parseDate = (e) =>{
+    setDate(e.target.value)
+    let yaga = e.target.value.split("/")
+    let newDate = {"Month": yaga[0], "Day": yaga[1], "Year": yaga[2]}
+    setInputField( {
+      ...inputField, 
+      "Date": newDate })  }
+
+
+  return (<>
+    <Paper style={{"border-style": "solid", margin:"25px", padding:"25px", backgroundColor:"#FAF9F6"}}>
+      <Grid container spacing={3}>
+        <Grid item xs={8}>
           <TextField
-            id="notes"
-            label="Notes about your run"
-            multiline
-            variant='filled'
-            rows={4}
+            style={{ "font-size": "400px"}}
+            id="Name"
+            label="Title of Run"
+            variant="standard"
+            size='large'
+            fullWidth 
+            inputProps={{style: {fontSize: 40}}}
+            onChange={inputsHandler} 
+            value={inputField.Name}
           />
-          </Grid>
-          <Grid item xs={12}  style={{display: 'flex',alignItems: 'center',  justifyContent: 'center'}}>
-            <Button variant="contained" size="large" style={{width: "180px", height: "60px"}}>
-              Submit
-            </Button>
-          </Grid>
+        </Grid>
+        <Grid item xs={4}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Type of Run</InputLabel>
+            <Select
+              labelId="typeOfRunLabel"
+              id="Type"
+              label="Type of Run"
+            >
+              <MenuItem value={"Training Run"}>Training Run</MenuItem>
+              <MenuItem value={"Long Run"}>Long Run</MenuItem>
+              <MenuItem value={"Race"}>Race</MenuItem>
+              <MenuItem value={"Workout"}>Workout</MenuItem>
+            </Select>
+          </FormControl>
         </Grid>
 
-      </Paper>
-    </>);
+        <Grid item xs={4} >
+          <TextField
+              id="Weather"
+              label="Weather"
+              autoComplete="current-password"
+              variant="filled"
+              onChange={inputsHandler} 
+              value={inputField.Weather}
+            />
+        </Grid>
+        <Grid item xs={8}  >
+          <TextField
+              id="date"
+              label="Date (MM/DD/YYYY)"
+              variant="filled"
+              value={date}
+              onChange={parseDate}
+            />
+        </Grid>
+        <Grid item xs={4}  >
+          <TextField
+              id="Miles"
+              label="Miles"
+              variant="filled"
+              onChange={inputsHandler} 
+              value={inputField.Miles}
+            />
+        </Grid>
+        <Grid item xs={4}  >
+          <TextField
+              id="Time"
+              label="Time (MM:SS)"
+              variant="filled"
+              value={time}
+              onChange={parseTime}
+            />
+        </Grid>
+        <Grid item xs={4}  >
+          <TextField
+              id="Pace"
+              label="Pace"
+              variant="filled"
+              disabled
+            />
+        </Grid>
+        <Grid item xs={4}  >
+          <TextField
+              id="AHR"
+              label="Average Heart Rate"
+              variant="filled"
+              onChange={inputsHandler} 
+              value={inputField.AHR}
+            />
+        </Grid>
+        <Grid item xs={4}  >
+          <TextField
+              id="BeforeRating"
+              label="Before Rating"
+              variant="filled"
+              onChange={inputsHandler} 
+              value={inputField.BeforeRating}
+            />
+        </Grid>
+        <Grid item xs={4}  >
+          <TextField
+              id="During Rating"
+              label="During Rating"
+              variant="filled"
+              onChange={inputsHandler} 
+              value={inputField.DuringRating}
+            />
+        </Grid>
+        <Grid item xs={4}  >
+          <TextField
+              id="Sleep"
+              label="Sleep HH:MM"
+              variant="filled"
+              value={sleepTime}
+              onChange={parseSleepTime}
+            />
+        </Grid>
+        <Grid item xs={4}  >
+          <TextField
+              id="RHR"
+              label="Resting Heart Rate"
+              variant="filled"
+              onChange={inputsHandler} 
+              value={inputField.RHR}
+            />
+        </Grid>
+        <Grid item xs={12}  >
+        <TextField
+          id="Notes"
+          label="Notes about your run"
+          multiline
+          variant='filled'
+          onChange={inputsHandler} 
+          value={inputField.Notes}
+          rows={4}
+        />
+        </Grid>
+        <Grid item xs={12}  style={{display: 'flex',alignItems: 'center',  justifyContent: 'center'}}>
+          <Button 
+            variant="contained" 
+            size="large"
+            onClick={submitButton} 
+            style={{width: "180px", height: "60px"}}>
+            Submit
+          </Button>
+        </Grid>
+      </Grid>
+
+    </Paper>
+  </>);
 };
+
+type Run = {
+  "Name": String,
+  "Type" ?: String,
+  "Pace" ?: {
+    "Minutes" ?: Number,
+    "Seconds" ?: Number
+  },
+  "Weather" ?: String,
+  "Miles" ?: Number,
+  "Time" ?: {
+    "Minutes" ?: Number,
+    "Seconds" ?: Number
+  },
+  "AHR" ?: Number,
+  "BeforeRating" ?: Number,
+  "DuringRating" ?: Number,
+  "Sleep" ?: {
+    "Hours" ?: Number,
+    "Minutes" ?: Number,
+  },
+  "RHR" ?: Number,
+  "Notes" ?: String,
+  "Date" ?: {
+    "Month": Number,
+    "Day": Number,
+    "Year": Number
+  }
+}
 
 export default UploadRun;
